14e41f3b5f048f9766e2bf915e923d6159193800
brunohdiass <bruno.dias22092007@gmail.com>
2025-08-07 11:08:09 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,35 @@
+let fotos = [];
+let indiceAtual = 0;
+
+function mostrarFoto() {
+  const foto = fotos[indiceAtual];
+  document.getElementById("foto").src = foto.imagem;
+  document.getElementById("descricao").textContent = foto.descricao;
+  document.getElementById("data").textContent = foto.data;
+}
+
+function avancar() {
+  if (indiceAtual < fotos.length - 1) {
+    indiceAtual++;
+    mostrarFoto();
+  }
+}
+
+function voltar() {
+  if (indiceAtual > 0) {
+    indiceAtual--;
+    mostrarFoto();
+  }
+}
+
+// Buscar as fotos da API
+fetch("http://localhost:3000/fotos")
+  .then(res => res.json())
+  .then(data => {
+    fotos = data;
+    mostrarFoto();
+  })
+  .catch(error => {
+    document.getElementById("descricao").textContent = "Erro ao carregar imagens.";
+    console.error("Erro na API:", error);
+  });
